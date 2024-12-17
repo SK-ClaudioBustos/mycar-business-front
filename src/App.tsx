8e13eeb394fa84c863ac552ef7ad7f7ad3f4c4bf
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-17 13:44:20 -0300

    styles: app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './App.css';
 
 function App() {
+  // const { loading, error } = useFetchToken(import.meta.env.VITE_EMAIL, import.meta.env.VITE_PASSWORD);
   return (
     <>
-
+      
     </>
   )
 }

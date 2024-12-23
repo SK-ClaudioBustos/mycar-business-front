d8591f023e1568927cd80dd79168cbc408c63207
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-23 10:38:34 -0300

    chore: credentials deleted on env example

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import '@styles/App.css';
 import '@styles/global.css';
 
 function App() {
-  // const { loading, error } = useFetchToken(import.meta.env.VITE_EMAIL, import.meta.env.VITE_PASSWORD);
   return (
     <Layout />
   )

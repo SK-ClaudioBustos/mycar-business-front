7bd617e313817d81e2bd4ce065503ead2fd0f52c
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-23 10:43:31 -0300

    feat: improve error component

--- src/utils/Error.tsx
+++ src/utils/Error.tsx
@@ -1,12 +1,26 @@
+import { ErrorData } from "@type/types";
 import "./styles/Error.css";
 
-export const Error = ({label}: {label: string}) => {
+interface Props {
+    label: string
+    error?: ErrorData
+}
+
+export const Error = ({ label, error }: Props) => {
     return (
         <div className="flex-center error-container">
             <div className="error box-shadow">
                 <h2>
                     {label}
                 </h2>
+                {
+                    error && (
+                        <>
+                            <p>{error.name}</p>
+                            <span>{error.message}</span>
+                        </>
+                    )
+                }
             </div>
         </div>
     );

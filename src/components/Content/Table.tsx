a974f3ead2c18b563e0fcbddebcf5c674a90c049
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-23 22:21:46 -0300

    refactor: retrieve rows from table context for improved state management

--- src/components/Content/Table.tsx
+++ src/components/Content/Table.tsx
@@ -1,72 +1,12 @@
-import { Car } from "@type/types";
+import { useTableContext } from "@context/table.context";
+import { Error } from "@utils/Error";
+import { Loading } from "@utils/Loading";
 import "./styles/Table.css";
 import { TableRow } from "./TableRow";
 
-const CARS: Car[] = [
-    {
-        id: "1",
-        company: "Renault",
-        model: "12",
-        KM: 1500
-    },
-    {
-        id: "2",
-        company: "Toyota",
-        model: "Corolla",
-        KM: 25000
-    },
-    {
-        id: "3",
-        company: "Ford",
-        model: "Focus",
-        KM: 32000
-    },
-    {
-        id: "4",
-        company: "Chevrolet",
-        model: "Onix",
-        KM: 18000
-    },
-    {
-        id: "5",
-        company: "Volkswagen",
-        model: "Golf",
-        KM: 45000
-    },
-    {
-        id: "6",
-        company: "Honda",
-        model: "Civic",
-        KM: 22000
-    },
-    {
-        id: "7",
-        company: "Hyundai",
-        model: "Elantra",
-        KM: 12000
-    },
-    {
-        id: "8",
-        company: "Nissan",
-        model: "Sentra",
-        KM: 27000
-    },
-    {
-        id: "9",
-        company: "BMW",
-        model: "3 Series",
-        KM: 55000
-    },
-    {
-        id: "10",
-        company: "Mercedes-Benz",
-        model: "C-Class",
-        KM: 40000
-    }
-];
-
-
 export const Table = () => {
+    const { tableRows, loadingTableRows, errorTableRows } = useTableContext();
+
     return (
         <div className="table box-shadow">
             <div className="table-header-container">
@@ -85,9 +25,26 @@ export const Table = () => {
             </div>
             <div className="table-body">
                 {
-                    CARS.map((car) => (
-                        <TableRow key={car.id} car={car} />
-                    ))
+                    loadingTableRows && (
+                        <div className="flex-center">
+                            <Loading label="Loading Cars" />
+                        </div>
+                    )
+                }
+                {
+                    errorTableRows && (
+                        <div className="flex-center">
+                            <Error label="An error has ocurred" error={errorTableRows} />
+                        </div>
+                    )
+                }
+                {
+                    tableRows.length > 0 && tableRows.map((car) => (
+                            <TableRow key={car.id} car={car} />
+                        ))
+                }
+                {
+                    !loadingTableRows && !errorTableRows && tableRows.length === 0 && (<div className="flex-center text-blue"><h3>No data for show</h3></div>)
                 }
             </div>
         </div>

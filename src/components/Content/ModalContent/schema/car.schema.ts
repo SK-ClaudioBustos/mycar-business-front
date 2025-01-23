6dc91fe53a6cdcd7f5e6e48fa842a0611bcb3c54
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-01-23 18:32:42 -0300

    fix: set the correct validation rules on schema

--- src/components/Content/ModalContent/schema/car.schema.ts
+++ src/components/Content/ModalContent/schema/car.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const carSchema = z.object({
     companyName: z.string().min(2, "At least 2 characters are required"),
-    modelName: z.string().min(3, "At least 3 characters are required."),
+    modelName: z.string().min(2, "At least 2 characters are required."),
     km: z.number().max(2000000, "The Maximum Kilometers permited are 2.000.000").optional()
 });
 

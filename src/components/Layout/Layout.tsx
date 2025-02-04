847659a4b38251a778100803f797b0a0b37151f2
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-04 10:34:29 -0300

    feat: enhance global error handling by relocating ErrorBoundary to main.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,26 +1,21 @@
 import { NavBar } from "@components/Navbar/NavBar";
 import { useAlertStorage } from "@store/alert.store";
-import { Error } from "@utils/Error";
 import { lazy } from "react";
 import { Outlet } from "react-router";
-import ErrorBoundary from "./ErrorBoundary";
 import "./Layout.css";
 const Alert = lazy(() => import("@utils/Alert"));
-
 
 export const Layout = () => {
     const alert = useAlertStorage((state) => state.alert);
     return (
-        <ErrorBoundary fallback={<Error label="ErrorBoundary: unknow error" />}>
-            <div className="layout">
-                <header>
-                    <NavBar />
-                </header>
-                <main>
-                    <Outlet />
-                    <Alert alert={alert} />
-                </main>
-            </div>
-        </ErrorBoundary>
+        <div className="layout">
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Outlet />
+                <Alert alert={alert} />
+            </main>
+        </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { AppRouter } from './routes/AppRouter';
+import ErrorBoundary from '@components/Layout/ErrorBoundary';
+import { Error } from '@utils/Error';
 
 createRoot(document.getElementById('root')!).render(
-  <AppRouter />
+  <ErrorBoundary fallback={<Error label="ErrorBoundary: unknow error" />}>
+    <AppRouter />
+  </ErrorBoundary>
 )

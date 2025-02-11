b60b674eb9c2c1152c0f73696bd9ff815a8e14c5
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-11 15:14:01 -0300

    refactor: update issues schema

--- src/components/Content/ModalContent/schema/issues.schema.ts
+++ src/components/Content/ModalContent/schema/issues.schema.ts
@@ -1,23 +1,23 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-    name: z.string(),
-    description: z.string(),
-    date: z.date(),
-    distance: z.number(),
-    car: z.string().array(),
-    status: z.string().array(),
-    type: z.string().array()
+    name: z.string().min(10).max(100),
+    description: z.string().min(25).max(250).optional(),
+    notificationDays: z.number().min(1).max(3650).optional(),
+    notificationDistance: z.number().min(50).max(15000).optional(),
+    currentDistance: z.number().min(0).max(2000000).optional(),
+    typeId: z.enum(["disabled","1","2"]),
+    carId: z.string().array(),
 });
 
 export const issueFormDefaultValues = {
     name: "",
     description: "",
-    date: "",
-    distance: 0,
-    car: undefined,
-    status: undefined,
-    type: undefined
+    notificationDays: 0,
+    notificationDistance: 0,
+    currentDistance: 0,
+    typeId: undefined,
+    carId: undefined,
 };
 
 export type IssueFormValues = z.infer<typeof issueSchema>;

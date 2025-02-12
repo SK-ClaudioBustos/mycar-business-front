7e546b046ebdce3e6874adcb13a720c0873ab117
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-12 11:35:31 -0300

    refactor: add useMemo for improve performance

--- src/components/IssuesSection/IssuesModalBody.tsx
+++ src/components/IssuesSection/IssuesModalBody.tsx
@@ -1,6 +1,6 @@
 import { useModalStorage } from "@store/modal.store";
 import { AppRoutes, ModalAction } from "@type/types";
-import { lazy, ReactNode } from "react";
+import { lazy, ReactNode, useMemo } from "react";
 import { getIssuesFormProps } from "./getIssuesFormProps";
 const Form = lazy(() => import("@components/Content/ModalContent/Form/Form"));
 const ItemDetails = lazy(() => import("@components/Content/ModalContent/ItemDetails/ItemDetails"));
@@ -9,12 +9,12 @@ const ConfirmDelete = lazy(() => import("@components/Content/ModalContent/Confir
 export const IssuesModalBody = () => {
     const action = useModalStorage((state) => state.modalData.action);
     const data = useModalStorage((state) => state.modalData.data);
-    const CONTENT_MAP: Record<ModalAction, ReactNode> = {
+    const CONTENT_MAP: Record<ModalAction, ReactNode> = useMemo(() => ({
         "showDetails": <ItemDetails section={AppRoutes.ISSUES} data={data} />,
         "create": <Form {...getIssuesFormProps("POST", data)} />,
         "edit": <Form {...getIssuesFormProps("PUT", data)} />,
         "delete": <ConfirmDelete data={data} />
-    };
+    }), [action, data]);
     return (
         <>
             {
